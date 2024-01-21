fa9f6dd937a2be7b4ae7ee54348e74927384c86c
dsd2900 <157327281+dsd2900@users.noreply.github.com>
2024-01-22 08:56:29 +0900

    Add files via upload

---
+++ precamp/class/03-javascript/01-variable.js
@@ -0,0 +1,5 @@
+const name = "최민하"
+
+name = "코딩"
+
+console.log(name)
---
+++ precamp/class/03-javascript/연습-04-object-in-array.js
@@ -0,0 +1,14 @@
+let classmates = [
+  {name:"철수", age: 13, school: "다람쥐초등학교"},
+  {name:"짱구", age: 13, school: "공룡초등학교"},
+  {name:"훈이", age: 13, school: "계곡초등학교"}
+]
+// undefined
+classmates
+// (3) [{…}, {…}, {…}]0: {name: '철수', age: 13, school: '다람쥐초등학교'}1: {name: '짱구', age: 13, school: '공룡초등학교'}2: {name: '훈이', age: 13, school: '계곡초등학교'}length: 3[[Prototype]]: Array(0)
+classmates.length
+// 3
+classmates[0].school
+// '다람쥐초등학교'
+classmates[2].age
+// 13
