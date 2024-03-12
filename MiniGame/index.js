3c3bef3a9d12594f5f9f9723412bd7176bba7db0
dsd2900 <157327281+dsd2900@users.noreply.github.com>
2024-03-12 10:52:22 +0900

    Add files via upload

---
+++ MiniGame/index.js
@@ -0,0 +1,39 @@
+if (game === "starting") {
+  const kill = console.log("누구를 죽이실 겁니까?")    
+
+  if(game.player === "kill") {
+    console.log("당신은 죽었습니다.")
+  }else if (doc.heal) {
+    console.log(누군가 당신을 살렸습니다.)
+  }
+
+  
+  const heal = {
+
+  }
+  const screen = {
+
+  }
+  const vote = {
+
+  }
+
+  let MafiaData = {
+    job:"Killer",
+    tool:"Gun" && "vote"
+  }
+
+  let DocData = {
+    job:"Doctor",
+    tool: "heal" && "vote"
+  }
+
+  let PolData = {
+    job:"police",
+    tool:"screen" && "vote"
+  }
+  let citData = {
+    job:"nothing",
+    tool:"vote"
+  }
+}
